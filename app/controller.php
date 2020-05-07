60d1d27cb7b04dff1fb54800fedf6e6e20b848b2
Danil Berezhnoy <moranilt@yahoo.com>
2020-05-07 12:56:50 +0300

    removed functions that interact with the USerModel class

--- app/controller.php
+++ app/controller.php
@@ -14,21 +14,5 @@ class Controller{
   public function users(){
     $this->model->getAllUsers();
   }
-
-  public function getUser($id){
-    $this->model->find($id);
-  }
-
-  public function newUser(){
-    $data = [
-      'firstname' => $_POST['firstname'],
-      'lastname' => $_POST['lastname'],
-      'login' => $_POST['login'],
-      'email' => $_POST['email'],
-      'password' => $_POST['password']
-    ];
-
-    return $this->model->create($data);
-  }
 }
  ?>
