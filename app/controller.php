6a4531121acbc391c84b6459d299410f22a3e420
Danil Berezhnoy <moranilt@yahoo.com>
2020-05-07 12:39:32 +0300

    added 2 functions to controll UserModel

--- app/controller.php
+++ app/controller.php
@@ -1,6 +1,7 @@
 <?php
 class Controller{
   private $model;
+  public $data = [];
 
   public function __construct($model){
     $this->model = $model;
@@ -12,6 +13,22 @@ class Controller{
 
   public function users(){
     $this->model->getAllUsers();
+  }
+
+  public function getUser($id){
+    $this->model->find($id);
+  }
+
+  public function newUser(){
+    $data = [
+      'firstname' => $_POST['firstname'],
+      'lastname' => $_POST['lastname'],
+      'login' => $_POST['login'],
+      'email' => $_POST['email'],
+      'password' => $_POST['password']
+    ];
+
+    return $this->model->create($data);
   }
 }
  ?>
