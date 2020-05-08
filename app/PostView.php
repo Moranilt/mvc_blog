034ff563862f70b4b1402a05cdd6a09f5ce47743
Danil Berezhnoy <moranilt@yahoo.com>
2020-05-08 14:18:40 +0300

    New PostView

---
+++ app/PostView.php
@@ -0,0 +1,17 @@
+<?php
+class PostView{
+  private $model;
+  private $template;
+
+  public function __construct($model, $template){
+    $this->model = $model;
+    $this->template = $template;
+  }
+
+  public function output(){
+    require($this->template);
+    return $this->model->posts;
+  }
+}
+
+ ?>
