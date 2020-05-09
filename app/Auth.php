36b574eed6ce48324f8e503b3a88cd5b3c4bb631
Danil Berezhnoy <moranilt@yahoo.com>
2020-05-09 21:19:49 +0300

    Added validation to login fields

--- app/Auth.php
+++ app/Auth.php
@@ -8,7 +8,7 @@ class Auth{
     $this->model = $model;
   }
 
-  function test_input($data2) {
+  private function test_input($data2) {
     $data2 = trim($data2);
     $data2 = stripslashes($data2);
     $data2 = htmlspecialchars($data2);
@@ -23,11 +23,17 @@ class Auth{
       ];
 
       $user = $this->model->find($data['login']);
-      if(password_verify($data['password'], $user['password'])){
-        session_start();
-        $_SESSION['user_login'] = $user['login'];
-        $_SESSION['user_id'] = $user['id'];
+      if(empty($user)){
+        echo "Login required";
+      }else{
+        if(password_verify($data['password'], $user['password'])){
+          $_SESSION['user_login'] = $user['login'];
+          $_SESSION['user_id'] = $user['id'];
+        }else{
+          echo "Password is incorrect";
+        }
       }
+
     }else{
       echo "You should complete the form. Login or Pass is empty.";
     }
@@ -38,6 +44,7 @@ class Auth{
     unset($_SESSION['user_login']);
     unset($_SESSION['user_id']);
     session_destroy();
+    header('Location: index.php?route=&action=');
   }
 }
 
