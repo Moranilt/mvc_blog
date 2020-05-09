20d5808dc80f4aa2f6213a6aa3f942f5251f2a55
Danil Berezhnoy <moranilt@yahoo.com>
2020-05-09 14:14:13 +0300

    Added function to validate values POST

--- app/UserController.php
+++ app/UserController.php
@@ -1,23 +1,31 @@
 <?php
 class UserController{
   private $model;
+  public $data2;
   public $data = [];
 
   public function __construct($model){
     $this->model = $model;
   }
-  
-  public function getUser($id){
-    $this->model->find($id);
+
+  public function getUser($login){
+    $this->model->find($login);
+  }
+
+  function test_input($data2) {
+    $data2 = trim($data2);
+    $data2 = stripslashes($data2);
+    $data2 = htmlspecialchars($data2);
+    return $data2;
   }
 
   public function newUser(){
     $data = [
-      'firstname' => $_POST['firstname'],
-      'lastname' => $_POST['lastname'],
-      'login' => $_POST['login'],
-      'email' => $_POST['email'],
-      'password' => $_POST['password']
+      'firstname' => self::test_input($_POST['firstname']),
+      'lastname' => self::test_input($_POST['lastname']),
+      'login' => self::test_input($_POST['login']),
+      'email' => self::test_input($_POST['email']),
+      'password' => self::test_input($_POST['password'])
     ];
 
     return $this->model->create($data);
