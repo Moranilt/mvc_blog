26131fd5bb493349cadb0753aff019444261f1a7
Danil Berezhnoy <moranilt@yahoo.com>
2020-05-09 14:12:41 +0300

    New form for LOGIN

--- tpl/all.php
+++ tpl/all.php
@@ -6,9 +6,28 @@
   <title>Rotue all</title>
  </head>
  <body>
+   <?php
+   if(!isset($_SESSION['user_login'])){
 
+     ?>
+     <form action="<?php echo setLink('login', 'login'); ?>" method="post">
+       <input type="text" name="login" value="" placeholder="Login" /><br />
+       <input type="password" name="password" value="" placeholder="Password" /><br />
+       <input type="submit" value="Sign In" />
+     </form>
+     <?php
+   }else{
+     echo 'User login - '.$_SESSION['user_login'].'. User id - '.$_SESSION['user_id'];
+
+     ?>
+     <a href="<?php echo setLink('logout', 'logout'); ?>">Logout</a>
+     <?php
+   }
+    ?>
   <!--<a href="index.php?page=tpl&criteria=lily&method=searchName">Link</a>-->
-  <a href="<?php echo setLink('user', 'getUser', '1'); ?>">Link</a>
+  <a href="<?php echo setLink('user', 'getUser', 'morani'); ?>">Link</a>
+
+
 
   <form action="<?php echo setLink('newuser', 'newUser'); ?>" method="post">
     <input type="text" name="firstname" value="" placeholder="firstname" /><br />
