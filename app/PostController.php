fa252a956e877a2261b7e42a0071004a47c97b07
Danil Berezhnoy <moranilt@yahoo.com>
2020-05-11 11:42:43 +0300

    Getting data from a form

--- app/PostController.php
+++ app/PostController.php
@@ -1,6 +1,8 @@
 <?php
 class PostController{
   private $model;
+  public $data = [];
+  public $data2;
 
   public function __construct($model){
     $this->model = $model;
@@ -13,6 +15,27 @@ class PostController{
   public function getPost($id){
     $this->model->show($id);
   }
-}
 
+  function test_input($data2) {
+    $data2 = trim($data2);
+    $data2 = stripslashes($data2);
+    $data2 = htmlspecialchars($data2);
+    return $data2;
+  }
+
+  public function create(){
+    $pdo = new \PDO('mysql:host='.DB_HOST.';dbname='.DB_SCHEMA, DB_USER, DB_PASS, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    $user = new UserModel($pdo);
+    $user_login = $_SESSION['user_login'];
+    $user = $user->find($user_login);
+
+      $data = [
+        'user_id' => $user['id'],
+        'title' => $_POST['title'],
+        'body' => $_POST['body']
+      ];
+      
+    return $this->model->store($data);
+  }
+}
  ?>
