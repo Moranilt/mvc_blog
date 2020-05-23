7e3df78553ec8f4c4cdbda0d7eed96d83bd6b48d
Danil Berezhnoy <moranilt@yahoo.com>
2020-05-23 19:54:53 +0300

    some changes to session

--- tpl/user.php
+++ tpl/user.php
@@ -7,7 +7,11 @@
  </head>
  <body>
   <?php
-  echo 'User lofin - '.$_SESSION['user_login'].'. User id - '.$_SESSION['user_id'];
+  if(isset($_SESSION['user_id'])){
+    echo 'User lofin - '.$_SESSION['user_login'].'. User id - '.$_SESSION['user_id'];
+  }
+
+
   echo $this->model->user["id"]." - ".$this->model->user["login"]." - ".$this->model->user["email"];
    ?>
 
